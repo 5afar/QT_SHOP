a6d53d1ab93c9aba3c7a595a2f5e34e883492617
Danila Safargaliev <SafarDaF@yandex.ru>
2023-06-01 12:11:14 +0300

    Прописал функционал кнопки покупки

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -63,14 +63,14 @@ void MainWindow::on_Shop_Button_clicked()
         isFull=true;
 
         QWidget *element= new QWidget ();
-        element->setFixedWidth(720);
         QHBoxLayout* layout = new QHBoxLayout(element);
 
         QLabel* labelimg = new QLabel();
         labelimg->setObjectName("image");
         labelimg->setSizePolicy(QSizePolicy::QSizePolicy::Maximum,QSizePolicy::Maximum);
         QPixmap pic = QPixmap(q2.value(0).toString());
-        QPixmap scaled = pic.scaled(QSize(640,360));
+        QPixmap scaled = pic.scaled(720,405,Qt::KeepAspectRatio);
+        labelimg->setFixedWidth(720);
         labelimg->setPixmap(scaled);
         layout->addWidget(labelimg);
 
@@ -139,7 +139,6 @@ void MainWindow::on_Library_Button_clicked()
     while(q1.next())
     {
         QWidget *element= new QWidget ();
-        element->setFixedWidth(720);
         QHBoxLayout* layout = new QHBoxLayout(element);
 
         QLabel* labelname = new QLabel();
@@ -165,31 +164,88 @@ void MainWindow::on_Library_Button_clicked()
 
 void MainWindow::buy_Button()
 {
-    onRemoveWidget();
     QPushButton* button=qobject_cast<QPushButton*>(sender());
     QHBoxLayout* layout=mButtonToLayoutMap.value(button);
+    QMessageBox::StandardButton reply;
+    QString s;
     if (layout){
         QLabel *label=qobject_cast<QLabel*>(layout->itemAt(1)->widget());
         if (label){
-            QString s=label->text();
-            QMessageBox::question(this,"buy","Вы хотите купить "+s+"?");
+            s=label->text();
+            reply = QMessageBox::question(this,"buy","Вы хотите купить "+s+"?", QMessageBox::Yes|QMessageBox::No);
         }
     }
 
+    if (reply==QMessageBox::Yes)
+    {
+        onRemoveWidget();
+        db.open();
+        QSqlQuery q1(QSqlDatabase::database("shop"));
+        QSqlQuery q2(QSqlDatabase::database("shop"));
+        q1.exec("SELECT * FROM content WHERE name='"+s+"'");
+
+        QWidget *wid=new QWidget;
+        QVBoxLayout* lay=new QVBoxLayout(wid);
+        QScrollArea *sc = new QScrollArea;
+        sc->setWidget(wid);
+        sc->setWidgetResizable(true);
+        QString idcontent;
+        QWidget *element= new QWidget ();
+        QHBoxLayout* layout = new QHBoxLayout(element);
+
+        while(q1.next())
+        {
+            idcontent=q1.value(0).toString();
+            q2.exec("SELECT link FROM content_img WHERE idcontent='"+idcontent+"'");
+            while(q2.next())
+            {
+                QLabel* labelimg = new QLabel();
+                labelimg->setObjectName("image");
+                labelimg->setSizePolicy(QSizePolicy::QSizePolicy::Maximum,QSizePolicy::Maximum);
+                QPixmap pic = QPixmap(q2.value(0).toString());
+                QPixmap scaled = pic.scaled(720,405,Qt::KeepAspectRatio);
+                labelimg->setFixedWidth(720);
+                labelimg->setPixmap(scaled);
+                layout->addWidget(labelimg);
+            }
+            QLabel* labelname = new QLabel();
+            labelname->setText("Название: "+q1.value(1).toString());
+            labelname->setObjectName("game");
+            labelname->setSizePolicy(QSizePolicy::QSizePolicy::Maximum,QSizePolicy::Maximum);
+            lay->addWidget(labelname);
+
+            QLabel* labelprice = new QLabel();
+            labelprice->setText("Цена: "+q1.value(2).toString()+"руб.");
+            labelprice->setSizePolicy(QSizePolicy::QSizePolicy::Maximum,QSizePolicy::Maximum);
+            lay->addWidget(labelprice);
+
+            QLabel* labelwallet = new QLabel();
+            labelwallet->setText("Ваш баланс: "+QString::number(user.GetWallet())+"руб.");
+            labelwallet->setSizePolicy(QSizePolicy::QSizePolicy::Maximum,QSizePolicy::Maximum);
+            lay->addWidget(labelwallet);
+
+        }
+
+
+        lay->addWidget(element);
+        ui->widgets_frame1->addWidget(sc);
+        lay->addStretch();
+        db.close();
+    }
 }
 
 void MainWindow::download_Button()
 {
     onRemoveWidget();
-    QPushButton* button=qobject_cast<QPushButton*>(sender());
-    QHBoxLayout* layout=mButtonToLayoutMap2.value(button);
-    if (layout){
-        QLabel *label=qobject_cast<QLabel*>(layout->itemAt(0)->widget());
-        if (label){
-            QString s=label->text();
-            QMessageBox::question(this,"buy","Вы хотите скачать "+s+"?");
-        }
-    }
+//    QPushButton* button=qobject_cast<QPushButton*>(sender());
+//    QHBoxLayout* layout=mButtonToLayoutMap2.value(button);
+//    if (layout){
+//        QLabel *label=qobject_cast<QLabel*>(layout->itemAt(0)->widget());
+//        if (label){
+//            QString s=label->text();
+//            QMessageBox::question(this,"buy","Вы хотите скачать "+s+"?");
+//        }
+//    }
 }
 
 
@@ -204,7 +260,6 @@ void MainWindow::on_Profile_Button_clicked()
     sc->setWidgetResizable(true);
 
     QWidget *element= new QWidget ();
-    element->setFixedWidth(720);
     QVBoxLayout* layout = new QVBoxLayout(element);
 
     QLabel* labelname = new QLabel();
